b0445a7821437fd4ae23ebc421af45ee6917fd8e
NayanaGPchandran <nayanakrish777@gmail.com>
2022-06-06 10:22:37 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  mynid: {
+    type: String,
+    default: 0,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+    business : {
+    type: Object,
+  },
+  vehicle : {
+    type: Object,
+  },
+  driver : {
+    type: Object,
+  },
+  paymentProfile : {
+    type: Object,
+  },
+  invoiceDetails : {
+    type: Object,
+  },
+  listingFee : {
+    type: Object,
+  },
+  created_by: {
+    type: String,
+    required: false,
+  },
+  comments : {
+    type: Object,
+  },
+  referral_code: {
+    type: String,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;

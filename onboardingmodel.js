f9b76ad854fe6ed1b96d9d8e288db90108d3645c
NayanaGPchandran <nayanakrish777@gmail.com>
2022-06-06 10:18:31 +0530

    Add files via upload

---
+++ onboardingmodel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const OnboardingSchema = new mongoose.Schema({
+  uuid: {
+    type: String,
+    required: true,
+  },
+  business : {
+    type: Object,
+  },
+  vehicle : {
+    type: Object,
+  },
+  driver : {
+    type: Object,
+  },
+  paymentProfile : {
+    type: Object,
+  },
+  invoiceDetails : {
+    type: Object,
+  },
+  listingFee : {
+    type: Object,
+  },
+  created_by: {
+    type: String,
+    required: false,
+  },
+  comments : {
+    type: Object,
+  },
+  referral_code: {
+    type: String,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  }
+});
+
+const Onboarding = mongoose.model("Onboarding", OnboardingSchema);
+module.exports = Onboarding;
